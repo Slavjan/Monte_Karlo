d0e33e2d637f43ac2c9eae067bd3b21f41dfba96
Slavjan <khaidayev@gmail.com>
2015-03-03 20:16:31 +0600

    reading paramaters from consol

--- ParametersReader.cpp
+++ ParametersReader.cpp
@@ -1,3 +1,51 @@
 #include "ParametersReader.h"
 
+ParametersReader::ParametersReader()
+{
+	path = '\0';
+}
+ParametersReader::~ParametersReader()
+{}
 
+void ParametersReader::parsing(int argc, char** argv)
+{
+	if (argc == 1)
+	{
+		throw "parameters wasn`t entering";
+		return;
+	}
+	else if (argc % 2 == 0)	 
+	{
+		throw "invalid parameters count";
+		return;
+	}	
+	else
+	{
+		for (int i = 1; i < argc; i += 2)
+		{
+			std::string  opt = std::string(argv[i]);
+			if (opt == "-f")
+			{
+				path = std::string(argv[i + 1]);
+				from = LOAD;
+				i++;
+			}
+			else if (opt == "-p")
+			{
+				k.push_back(atoi(argv[i]));
+				k.push_back(atoi(argv[i + 1]));
+				from = PARAMS;
+			}
+		}
+	}
+}
+
+std::vector <int> ParametersReader::getVector()
+{
+	return k;
+}
+
+std::string ParametersReader::getPath()
+{
+	return path;
+}
--- ParametersReader.h
+++ ParametersReader.h
@@ -2,11 +2,22 @@
 #include <vector>
 #include <iostream>
 
+#define LOAD 0;
+#define PARAMS 1;
+bool from;
+
 class ParametersReader
 {
 private:
 	std::vector <int> k;
-
+	std::string path;
 public:
+	ParametersReader();
+	~ParametersReader();
+
+	void parsing(int argc, char** argv);
+
+	std::vector <int> getVector();
+	std::string getPath();
 };
 
