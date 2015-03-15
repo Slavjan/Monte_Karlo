0bc249a7152f3bf26e525110145ce99283fa42b3
Slavjan <khaidayev@gmail.com>
2015-03-15 21:36:46 +0600

    trace for symbol '-'

--- ParametersReader.cpp
+++ ParametersReader.cpp
@@ -1,9 +1,6 @@
 #include "ParametersReader.h"
 // public
-
-
-
-
+		   
 ParametersReader::ParametersReader(int _argc, char** _argv)
 {
 	argc = _argc;
@@ -21,7 +18,7 @@ string ParametersReader::findString(string opt)
 			string arg = string(argv[i]);
 			if (
 				arg == opt && 
-				!string(argv[i + 1]).begin('-') && 
+				!(string(argv[i + 1])[0] == '-') && 
 				i + 1 <= argc
 			   )
 				return string(argv[i+1]);
@@ -30,7 +27,6 @@ string ParametersReader::findString(string opt)
 	else
 	{
 		throw Empty();
-		return;
 	}
 }
 
@@ -52,9 +48,7 @@ std::vector<int> ParametersReader::findVector(string opt)
 				if (v.size() % 2 != 0)
 				{
 					throw Invalid();
-					return;
-				}
-
+				}		  
 				return v;
 			}
 			else
@@ -65,119 +59,5 @@ std::vector<int> ParametersReader::findVector(string opt)
 	else
 	{
 		throw Empty();
-		return;
 	}
 }
-
-//void ParametersReader::parsing(int argc, char** argv)
-//{
-//	if (argc == 1)
-//	{
-//		throw Empty();
-//		return;
-//	}
-//	else if (argc % 2 == 0)	 
-//	{
-//		throw Invalid();
-//		return;
-//	}	
-//	else
-//	{
-//		for (int i = 1; i < argc; i += 2)
-//		{
-//			std::string  opt = std::string(argv[i]);
-//			if (opt == "-i") // from file - input
-//			{
-//				pathFrom = std::string(argv[i + 1]);
-//				i++;
-//			}
-//			else if (opt == "-p") // points
-//			{
-//				while (atoi(argv[i]) && atoi(argv[i + 1]))
-//				{
-//					k.push_back(atoi(argv[i]));
-//					k.push_back(atoi(argv[i + 1]));
-//					i++;
-//				}
-//			}
-//			else if (opt == "-o") // to file - output
-//			{
-//				pathTo = std::string(argv[i + 1]);
-//				i++;
-//			}
-//		}
-//	}
-//}
-//	// getting
-//
-//std::vector<int> ParametersReader::getVector()
-//{
-//	return coords;
-//}
-//
-//std::string ParametersReader::getPathFrom()
-//{
-//	return pathFrom;
-//}
-//
-//std::string ParametersReader::getPathTo()
-//{
-//	return pathTo;
-//}
-//	// /getting
-//// /public
-//
-//// private
-//	// finding params
-//std::string ParametersReader::findInputPath(int argc, char** argv)
-//{
-//	for (int i = 1; i < argc; i++)
-//	{
-//		std::string  opt = std::string(argv[i]);
-//		if (opt == "-i") // from file - input
-//		{
-//			pathFrom = std::string(argv[i + 1]);
-//		}
-//	}
-//	
-//	return pathFrom;
-//}
-//
-//std::string ParametersReader::findOutputPath(int argc, char** argv)
-//{
-//	for (int i = 1; i < argc; i++)
-//	{
-//		std::string  opt = std::string(argv[i]);
-//		if (opt == "-o") // to file - output
-//		{
-//			pathTo = std::string(argv[i + 1]);
-//		}
-//	}
-//	
-//	return pathTo;
-//}
-//
-//std::string ParametersReader::findEnteredCoords(int argc, char** argv)
-//{
-//	std::string str;
-//	
-//	for (int i = 1; i < argc; i++)
-//	{
-//		std::string  opt = std::string(argv[i]);
-//		if (opt == "-p") // point
-//		{
-//			str = std::string(argv[i + 1]);
-//		}
-//	}
-//
-//	
-//	return str;
-//}
-//
-//std::vector<int> ParametersReader::parsCoords()
-//{
-//	
-//	
-//	return 0;
-//}
-
