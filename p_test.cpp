16e204cbea343e73e6c823c50988691a1d01a98b
Slavjan <khaidayev@gmail.com>
2015-03-22 19:29:34 +0600

    report, redact solution

---
+++ p_test.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "ParametersReader.h"
+
+
+int main(int argc, char** argv)
+{
+	
+
+
+#ifdef _MSC_VER // if compiling in the MS Visual Studio
+	system("pause");
+#endif
+
+	return 0;
+}
+
+
