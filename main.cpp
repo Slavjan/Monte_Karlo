8075c1417fb7aee999bfd426cb8045d0da393c9f
Slavjan <khaidayev@gmail.com>
2015-03-24 13:12:57 +0600

    testing params

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #pragma once
 #include "stdafx.h"
 #include "myApp.h"
+#include <stdio.h>
 
 
 int main(int argc, char** argv)
 {
+	argv[0] = "Monte_Karlo.exe";
+	argv[1] = "-p";
+	argv[2] = "0";
+	argv[3] = "0";
+	argv[4] = "0";
+	argv[5] = "10";
+	argv[6] = "10";
+	argv[7] = "10";
+	argv[8] = "10"; 
+	argv[9] = "0";
+	argc = 10;
+
 	myApp App = myApp(argc, argv);
 	bool ex;
 	
